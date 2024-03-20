eea78d8d7a681794f47924474a4443eda38d243b
ripa <22086553+ripperi@users.noreply.github.com>
2024-03-20 19:59:23 +0200

    Upload component

---
+++ ui/src/components/Upload.jsx
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import { upload } from "/src/api";
+
+export default function Upload({ className = "", children, slugs = [] }) {
+  const formRef = useRef(null);
+  const fileRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const file = fileRef.current.files[0];
+    upload(slugs.concat(file.name), file);
+  };
+
+  return (
+    <form className="inline" ref={formRef} onSubmit={handleSubmit}>
+      <label htmlFor="file-upload" className={className}>
+        {...children}
+      </label>
+      <input
+        className="hidden"
+        id="file-upload"
+        ref={fileRef}
+        type="file"
+        multiple
+        onChange={(e) => {
+          formRef.current.dispatchEvent(
+            new Event("submit", { cancelable: true, bubbles: true })
+          );
+          fileRef.current.value = null;
+        }}
+      />
+    </form>
+  );
+}
