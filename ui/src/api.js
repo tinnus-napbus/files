4b0a89391aa009a3e6b5247a299d64d48623279f
ripa <22086553+ripperi@users.noreply.github.com>
2024-03-20 19:58:00 +0200

    upload

--- ui/src/api.js
+++ ui/src/api.js
@@ -4,4 +4,21 @@ import axios from "axios";
 const api = new Urbit("", "", window.desk || "files");
 api.ship = window.ship;
 
+export async function upload(slugs, file) {
+  await axios.post(`/files-upload/${slugs.join("/")}`, file, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": file.type,
+    },
+    onUploadProgress: (progressEvent) => {
+      const progress = (progressEvent.loaded / progressEvent.total) * 100;
+      console.log(progress);
+    },
+    onDownloadProgress: (progressEvent) => {
+      const progress = (progressEvent.loaded / progressEvent.total) * 100;
+      console.log(progress);
+    },
+  });
+}
+
 export default api;
