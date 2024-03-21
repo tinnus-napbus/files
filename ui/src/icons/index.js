5cdf24a83c87241bcae41699392c4c5f8f93da8c
ripa <22086553+ripperi@users.noreply.github.com>
2024-03-21 14:25:49 +0200

    Folder icon

--- ui/src/icons/index.js
+++ ui/src/icons/index.js
@@ -1,3 +1,4 @@
+export { default as Folder } from "./Folder";
 export { default as NewFolderIcon } from "./NewFolder";
 export { default as SearchIcon } from "./Search";
 export { default as UploadIcon } from "./Upload";
